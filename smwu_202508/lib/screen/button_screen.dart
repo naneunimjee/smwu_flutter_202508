972a3e2ad6a6c59cb7301d620f9cb5cf57ad0090
임지영 <141422840+naneunimjee@users.noreply.github.com>
2025-08-21 02:41:56 +0900

    Feat: 스택, 컬럼&로우 실습, 스크롤바, 이미지, 텍스트 화면으로 넘어가기 위한 버튼 추가

--- smwu_202508/lib/screen/button_screen.dart
+++ smwu_202508/lib/screen/button_screen.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:smwu_202508/screen/Text/text_practice_screen.dart';
+import 'package:smwu_202508/screen/Text/text_screen.dart';
 import 'package:smwu_202508/screen/column/column_practice_screen.dart';
 import 'package:smwu_202508/screen/column/column_screen.dart';
 import 'package:smwu_202508/screen/container/container_practice_screen.dart';
 import 'package:smwu_202508/screen/container/container_screen.dart';
+import 'package:smwu_202508/screen/image/image_practice_screen.dart';
+import 'package:smwu_202508/screen/image/image_screen.dart';
+import 'package:smwu_202508/screen/row/column_row_practice_screen.dart';
+import 'package:smwu_202508/screen/row/row_practice_screen.dart';
 import 'package:smwu_202508/screen/row/row_screen.dart';
+import 'package:smwu_202508/screen/scrollview/scroll_view_screen.dart';
+import 'package:smwu_202508/screen/stack/stack_practice_screen.dart';
+import 'package:smwu_202508/screen/stack/stack_screen.dart';
 
 class ButtonScreen extends StatelessWidget {
   const ButtonScreen({super.key});
@@ -84,6 +93,143 @@ class ButtonScreen extends StatelessWidget {
               },
               child: Text("row"),
             ),
+
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return RowPracticeScreen();
+                    },
+                  ),
+                );
+              },
+              child: Text("row 실습"),
+            ),
+
+
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return ColumnRowPracticeScreen();
+                    },
+                  ),
+                );
+              },
+              child: Text("Column, Row 실습"),
+            ),
+
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return TextScreen();
+                    },
+                  ),
+                );
+              },
+              child: Text("Text"),
+            ),
+
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return TextPracticeScreen();
+                    },
+                  ),
+                );
+              },
+              child: Text("Text 실습"),
+            ),
+
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return ImageScreen();
+                    },
+                  ),
+                );
+              },
+              child: Text("image"),
+            ),
+
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return ImagePracticeScreen();
+                    },
+                  ),
+                );
+              },
+              child: Text("image 실습"),
+            ),
+
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return StackScreen();
+                    },
+                  ),
+                );
+              },
+              child: Text("stack"),
+            ),
+
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return StackPracticeScreen();
+                    },
+                  ),
+                );
+              },
+              child: Text("stack practice"),
+            ),
+
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return ScrollViewScreen();
+                    },
+                  ),
+                );
+              },
+              child: Text("scroll view"),
+            ),
+
           ],
         ),
       ),
