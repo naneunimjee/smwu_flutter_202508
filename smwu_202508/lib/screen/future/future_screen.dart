89e97af336f766616aaf1796d1dc01d7f61db640
임지영 <141422840+naneunimjee@users.noreply.github.com>
2025-08-28 00:29:19 +0900

    Feat: 동기 & 비동기 학습

---
+++ smwu_202508/lib/screen/future/future_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class FutureScreen extends StatefulWidget {
+  const FutureScreen({super.key});
+
+  @override
+  State<FutureScreen> createState() => _FutureScreenState();
+}
+
+class _FutureScreenState extends State<FutureScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(title: Text("Future")),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          ElevatedButton(onPressed: () async {
+            print("시작 전");
+
+            String value = await futureBasic();
+
+            // await Future.delayed(Duration(milliseconds: 1000), () {
+            //   // 여기에 실행할 코드 작성
+            //   print("딜레이");
+            // });
+            print("시작 후 : $value");
+            
+          }, child: Text("Delay"))
+
+        ],
+      ),);
+  }
+}
+
+
+
+/// Future를 리턴하는 경우 Future 리턴
+/// Future를 리턴하는 경우가 아니면
+/// Future를 리턴하는 함수는 then을 활용할 수 있음
+Future<String> futureBasic() async {
+  await Future.delayed(Duration(milliseconds: 1500));
+  return "result";
+}
