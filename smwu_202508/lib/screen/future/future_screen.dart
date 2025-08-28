03979539cee8a8b328520fdaf4f7d42b1a933545
임지영 <141422840+naneunimjee@users.noreply.github.com>
2025-08-29 02:00:36 +0900

    Comment: future_screen 파일 동기&비동기 설명 추가

--- smwu_202508/lib/screen/future/future_screen.dart
+++ smwu_202508/lib/screen/future/future_screen.dart
@@ -19,6 +19,7 @@ class _FutureScreenState extends State<FutureScreen> {
 
             String value = await futureBasic();
 
+            /// 함수 호출을 선언과 동시에
             // await Future.delayed(Duration(milliseconds: 1000), () {
             //   // 여기에 실행할 코드 작성
             //   print("딜레이");
@@ -35,7 +36,7 @@ class _FutureScreenState extends State<FutureScreen> {
 
 
 /// Future를 리턴하는 경우 Future 리턴
-/// Future를 리턴하는 경우가 아니면
+/// Future를 리턴하는 경우가 아니면 에러를 리턴
 /// Future를 리턴하는 함수는 then을 활용할 수 있음
 Future<String> futureBasic() async {
   await Future.delayed(Duration(milliseconds: 1500));
